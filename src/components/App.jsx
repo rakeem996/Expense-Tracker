3fc248e13c03e3d1e5c0c7e754268be92549ae34
Rakeem <rakeemshaikh4545@gmail.com>
2023-03-23 11:17:20 +0530

    creating the components and the structure of the app

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import Balance from "./Balance";
+import IncomeExpense from "./IncomeExpense";
+import TransactionList from "./TransactionList";
+import AddTransaction from "./AddTransaction";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense/>
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
+}
+
+export default App;
